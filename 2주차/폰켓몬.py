75473bfe6368c19ebbe535fe5ceeb9372f1f458e
이건희 <51435556+good9804@users.noreply.github.com>
2023-04-13 18:40:35 +0900

    Update 폰켓몬.py

--- 2주차/폰켓몬.py
+++ 2주차/폰켓몬.py
@@ -1,8 +1,3 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-<dict>
-	<key>URL</key>
-	<string>https://github.com/good9804/SolvingAlgorithm/blob/main/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4/lv1/1845.%E2%80%85%ED%8F%B0%EC%BC%93%EB%AA%AC/%ED%8F%B0%EC%BC%93%EB%AA%AC.py</string>
-</dict>
-</plist>
+def solution(nums):
+    return min(len(nums)/2, len(set(nums)))
+    ##중복되지않은것이  N/2보다 작으면 정답,
